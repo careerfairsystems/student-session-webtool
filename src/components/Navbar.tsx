127fc4ed3418feaf6324904e0ddd5544d66cbb33
Stronkness <andrefrisk98@gmail.com>
2023-09-08 11:40:11 +0200

    Change header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,33 @@ import { API } from "../api";
 import { AuthContext } from "./AuthContext";
 
 export default function Navbar() {
-  const [loading, setLoading] = useState(false);
-  const authContext = useContext(AuthContext);
-  
-  const logout = async () => {
-    setLoading(true);
-    await API.auth.logout();
-    authContext.signOut();
-    setLoading(false);
-  }
-  return (
-    <HStack w="100%" px="1rem" py="0.5rem" justifyContent="space-between" alignItems="center">
-      <Heading fontSize="xl">
-        Arkad
-      </Heading>
-      <Button onClick={logout} variant="primary" fontSize="sm" isLoading={loading} p="1.5rem">Sign Out</Button>
-    </HStack>
-  )
+	const [loading, setLoading] = useState(false);
+	const authContext = useContext(AuthContext);
+
+	const logout = async () => {
+		setLoading(true);
+		await API.auth.logout();
+		authContext.signOut();
+		setLoading(false);
+	};
+	return (
+		<HStack
+			w="100%"
+			px="1rem"
+			py="0.5rem"
+			justifyContent="space-between"
+			alignItems="center"
+		>
+			<Heading fontSize="xl">ARKAD</Heading>
+			<Button
+				onClick={logout}
+				variant="primary"
+				fontSize="sm"
+				isLoading={loading}
+				p="1.5rem"
+			>
+				Sign Out
+			</Button>
+		</HStack>
+	);
 }
