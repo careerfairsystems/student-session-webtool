976abaae9cb0d2ebb6215a988c23a03f46aa72cd
Stronkness <andrefrisk98@gmail.com>
2023-11-04 14:48:02 +0100

    Fix video and reject button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,68 +4,71 @@ import { API } from "../api";
 import { AuthContext } from "./AuthContext";
 
 export default function Navbar() {
-	const [loading, setLoading] = useState(false);
-	const [guideText, setGuideText] = useState("View Guide");
-	const [showVideo, setShowVideo] = useState(false); // State to control video visibility
-	const authContext = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+  const [guideText, setGuideText] = useState("View Guide");
+  const [showVideo, setShowVideo] = useState(false); // State to control video visibility
+  const authContext = useContext(AuthContext);
 
-	const logout = async () => {
-		setLoading(true);
-		await API.auth.logout();
-		authContext.signOut();
-		setLoading(false);
-	};
+  const logout = async () => {
+    setLoading(true);
+    await API.auth.logout();
+    authContext.signOut();
+    setLoading(false);
+  };
 
-	const guide = async () => {
-		setLoading(true);
-		if (guideText === "View Guide") {
-			setGuideText("Hide Guide");
-			setShowVideo(true); // Show the video
-		} else {
-			setGuideText("View Guide");
-			setShowVideo(false); // Hide the video
-		}
-		setLoading(false);
-	};
+  const guide = async () => {
+    setLoading(true);
+    if (guideText === "View Guide") {
+      setGuideText("Hide Guide");
+      setShowVideo(true); // Show the video
+    } else {
+      setGuideText("View Guide");
+      setShowVideo(false); // Hide the video
+    }
+    setLoading(false);
+  };
 
-	return (
-		<>
-			<HStack
-				w="100%"
-				px="1rem"
-				py="0.5rem"
-				justifyContent="space-between"
-				alignItems="center"
-			>
-				<Heading fontSize="xl">ARKAD</Heading>
-				<Flex>
-					<Button
-						onClick={guide}
-						variant="primary"
-						fontSize="sm"
-						isLoading={loading}
-						p="1.5rem"
-					>
-						{guideText}
-					</Button>
-					<Button
-						onClick={logout}
-						variant="primary"
-						fontSize="sm"
-						isLoading={loading}
-						p="1.5rem"
-						marginLeft="1rem"
-					>
-						Sign Out
-					</Button>
-				</Flex>
-			</HStack>
-			{showVideo && (
-				<video controls>
-					<source src="../../hd1967.mp4" type="video/mp4" />
-					Your browser does not support the video tag.
-				</video>
-			)}
-		</>
-	);
+  return (
+    <>
+      <HStack
+        w="100%"
+        px="1rem"
+        py="0.5rem"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Heading fontSize="xl">ARKAD</Heading>
+        <Flex>
+          <Button
+            onClick={guide}
+            variant="primary"
+            fontSize="sm"
+            isLoading={loading}
+            p="1.5rem"
+          >
+            {guideText}
+          </Button>
+          <Button
+            onClick={logout}
+            variant="primary"
+            fontSize="sm"
+            isLoading={loading}
+            p="1.5rem"
+            marginLeft="1rem"
+          >
+            Sign Out
+          </Button>
+        </Flex>
+      </HStack>
+      {showVideo && (
+        <video controls>
+          <source
+            src="https://cvfiler.s3.eu-north-1.amazonaws.com/studentsessions.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      )}
+    </>
+  );
 }
--- src/components/StudentModal.tsx
+++ src/components/StudentModal.tsx
@@ -1,22 +1,22 @@
 import {
-	Button,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	HStack,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Image,
-	Flex,
-	Text,
-	Box,
-	Link,
-	SimpleGrid,
-	Skeleton,
-	SkeletonText,
-	VStack,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  HStack,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Image,
+  Flex,
+  Text,
+  Box,
+  Link,
+  SimpleGrid,
+  Skeleton,
+  SkeletonText,
+  VStack,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { API } from "../api";
@@ -26,204 +26,194 @@ import { User } from "../api/users";
 import StudentInfoItem from "./StudentInfoItem";
 
 export type StudentModalProps = {
-	isOpen: boolean;
-	onClose: () => void;
-	studentId: number;
+  isOpen: boolean;
+  onClose: () => void;
+  studentId: number;
 };
 
 export default function StudentModal({
-	isOpen,
-	onClose,
-	studentId,
+  isOpen,
+  onClose,
+  studentId,
 }: StudentModalProps) {
-	const [application, setApplication] = useState<SSApplication | null>(null);
-	const [student, setStudent] = useState<Student | null>(null);
-	const [user, setUser] = useState<User | null>(null);
-	const [loading, setLoading] = useState<boolean>(true);
-	const [cvUri, setCvUri] = useState<string | null>(null);
+  const [application, setApplication] = useState<SSApplication | null>(null);
+  const [student, setStudent] = useState<Student | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [cvUri, setCvUri] = useState<string | null>(null);
 
-	async function getAppAndStudent() {
-		setLoading(true);
-		const sdnt = await API.students.getStudent(studentId);
-		const app = await API.sSApplications.getApplicationForStudent(sdnt.id);
-		const user = await API.users.getUser(sdnt.userId);
-		const uri = await API.s3bucket.getFromS3(user.id.toString(), ".pdf");
-		setApplication(app ?? null);
-		setStudent(sdnt);
-		setUser(user);
-		setLoading(false);
-		if (uri.ok) {
-			setCvUri(uri.url);
-		}
-	}
-	async function accept() {
-		setLoading(true);
-		application &&
-			(await API.sSApplications.changeApplication(application.id, {
-				status: 1,
-			} as UpdateApplicationDto));
-		await getAppAndStudent();
-		onClose();
-		setLoading(false);
-	}
-	async function pending() {
-		setLoading(true);
-		application &&
-			(await API.sSApplications.changeApplication(application.id, {
-				status: 0,
-			} as UpdateApplicationDto));
-		await getAppAndStudent();
-		onClose();
-		setLoading(false);
-	}
-	async function reject() {
-		setLoading(true);
-		application &&
-			(await API.sSApplications.changeApplication(application.id, {
-				status: 2,
-			} as UpdateApplicationDto));
-		await getAppAndStudent();
-		onClose();
-		setLoading(false);
-	}
+  async function getAppAndStudent() {
+    setLoading(true);
+    const sdnt = await API.students.getStudent(studentId);
+    const app = await API.sSApplications.getApplicationForStudent(sdnt.id);
+    const user = await API.users.getUser(sdnt.userId);
+    const uri = await API.s3bucket.getFromS3(user.id.toString(), ".pdf");
+    setApplication(app ?? null);
+    setStudent(sdnt);
+    setUser(user);
+    setLoading(false);
+    if (uri.ok) {
+      setCvUri(uri.url);
+    }
+  }
+  async function accept() {
+    setLoading(true);
+    application &&
+      (await API.sSApplications.changeApplication(application.id, {
+        status: 1,
+      } as UpdateApplicationDto));
+    await getAppAndStudent();
+    onClose();
+    setLoading(false);
+  }
+  async function pending() {
+    setLoading(true);
+    application &&
+      (await API.sSApplications.changeApplication(application.id, {
+        status: 0,
+      } as UpdateApplicationDto));
+    await getAppAndStudent();
+    onClose();
+    setLoading(false);
+  }
+  async function reject() {
+    setLoading(true);
+    application &&
+      (await API.sSApplications.changeApplication(application.id, {
+        status: 2,
+      } as UpdateApplicationDto));
+    await getAppAndStudent();
+    onClose();
+    setLoading(false);
+  }
 
-	useEffect(() => {
-		getAppAndStudent();
-	}, []);
+  useEffect(() => {
+    getAppAndStudent();
+  }, []);
 
-	console.log(user?.profilePictureUrl);
-	return (
-		<Modal isOpen={isOpen} onClose={onClose}>
-			<ModalOverlay />
-			<ModalContent>
-				<ModalHeader>
-					<HStack justifyContent="space-between" mr="2rem">
-						<Box>
-							<Image
-								w="5.5rem"
-								h="5.5rem"
-								rounded="full"
-								src={
-									user?.profilePictureUrl
-										? user?.profilePictureUrl
-										: "public/images/arkad_logo.png"
-								}
-							/>
-							<Text>
-								<Skeleton isLoaded={!loading}>
-									{user?.firstName ?? " "}{" "}
-									{user?.lastName ?? " "}
-								</Skeleton>
-							</Text>
-						</Box>
-						{student?.linkedIn ? (
-							<Button
-								variant="secondary"
-								p="1rem"
-								as="a"
-								href={student.linkedIn}
-								target="_blank"
-								rel="noreferrer"
-							>
-								LinkedIn
-							</Button>
-						) : (
-							<Text> No LinkedIn</Text>
-						)}
-					</HStack>
-				</ModalHeader>
-				<ModalCloseButton />
-				<ModalBody>
-					<Flex justifyContent="space-between" flexDir="column">
-						<StudentInfoItem
-							loading={loading}
-							label="Email"
-							value={user?.email ?? "no email"}
-						/>
-						<StudentInfoItem
-							loading={loading}
-							label="Phone"
-							value={user?.phoneNr ?? "No phone number"}
-						/>
-						<StudentInfoItem
-							loading={loading}
-							label="Program"
-							value={
-								student?.programme
-									? student?.programme &&
-									  Programme[student.programme].replaceAll(
-											"_",
-											" "
-									  )
-									: "No program"
-							}
-						/>
-						<StudentInfoItem
-							loading={loading}
-							label="Year"
-							value={String(student?.year ?? "No year")}
-						/>
-						<StudentInfoItem
-							loading={loading}
-							label="Master"
-							value={student?.masterTitle ?? "No master"}
-						/>
-						{user && (
-							<Skeleton isLoaded={!loading}>
-								<VStack align="strech" spacing="0" my="0.2rem">
-									<Text fontWeight={700}>CV: </Text>
-									{cvUri ? (
-										<Link
-											color="ArkadLightBlue"
-											href={cvUri}
-										>
-											Download CV
-										</Link>
-									) : (
-										<Text>CV not uploaded</Text>
-									)}
-								</VStack>
-							</Skeleton>
-						)}
-						<Box rounded="md" mt="1rem">
-							<Skeleton isLoaded={!loading}>
-								<Text fontWeight={700} fontSize="1.2rem">
-									Message from student
-								</Text>
-							</Skeleton>
-							<SkeletonText isLoaded={!loading}>
-								<Text>{application?.motivation ?? " "}</Text>
-							</SkeletonText>
-						</Box>
-					</Flex>
-				</ModalBody>
+  console.log(user?.profilePictureUrl);
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <HStack justifyContent="space-between" mr="2rem">
+            <Box>
+              <Image
+                w="5.5rem"
+                h="5.5rem"
+                rounded="full"
+                src={
+                  user?.profilePictureUrl
+                    ? user?.profilePictureUrl
+                    : "public/images/arkad_logo.png"
+                }
+              />
+              <Text>
+                <Skeleton isLoaded={!loading}>
+                  {user?.firstName ?? " "} {user?.lastName ?? " "}
+                </Skeleton>
+              </Text>
+            </Box>
+            {student?.linkedIn ? (
+              <Button
+                variant="secondary"
+                p="1rem"
+                as="a"
+                href={student.linkedIn}
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </Button>
+            ) : (
+              <Text> No LinkedIn</Text>
+            )}
+          </HStack>
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Flex justifyContent="space-between" flexDir="column">
+            <StudentInfoItem
+              loading={loading}
+              label="Email"
+              value={user?.email ?? "no email"}
+            />
+            <StudentInfoItem
+              loading={loading}
+              label="Phone"
+              value={user?.phoneNr ?? "No phone number"}
+            />
+            <StudentInfoItem
+              loading={loading}
+              label="Program"
+              value={
+                student?.programme
+                  ? student?.programme &&
+                    Programme[student.programme].replaceAll("_", " ")
+                  : "No program"
+              }
+            />
+            <StudentInfoItem
+              loading={loading}
+              label="Year"
+              value={String(student?.year ?? "No year")}
+            />
+            <StudentInfoItem
+              loading={loading}
+              label="Master"
+              value={student?.masterTitle ?? "No master"}
+            />
+            {user && (
+              <Skeleton isLoaded={!loading}>
+                <VStack align="strech" spacing="0" my="0.2rem">
+                  <Text fontWeight={700}>CV: </Text>
+                  {cvUri ? (
+                    <Link color="ArkadLightBlue" href={cvUri}>
+                      Download CV
+                    </Link>
+                  ) : (
+                    <Text>CV not uploaded</Text>
+                  )}
+                </VStack>
+              </Skeleton>
+            )}
+            <Box rounded="md" mt="1rem">
+              <Skeleton isLoaded={!loading}>
+                <Text fontWeight={700} fontSize="1.2rem">
+                  Message from student
+                </Text>
+              </Skeleton>
+              <SkeletonText isLoaded={!loading}>
+                <Text>{application?.motivation ?? " "}</Text>
+              </SkeletonText>
+            </Box>
+          </Flex>
+        </ModalBody>
 
-				<ModalFooter>
-					<SimpleGrid columns={3} spacing="1rem" w="100%">
-						<Button variant="primary" onClick={onClose}>
-							Close
-						</Button>
-						{(application?.status === 2 ||
-							application?.status === 0) && (
-							<Button variant="accept" onClick={accept}>
-								Accept
-							</Button>
-						)}
-						{(application?.status === 2 ||
-							application?.status === 1) && (
-							<Button variant="secondary" onClick={pending}>
-								Pending
-							</Button>
-						)}
-						{(application?.status === 1 ||
-							application?.status === 0) && (
-							<Button variant="decline" onClick={reject}>
-								Reject
-							</Button>
-						)}
-					</SimpleGrid>
-				</ModalFooter>
-			</ModalContent>
-		</Modal>
-	);
+        <ModalFooter>
+          <SimpleGrid columns={3} spacing="1rem" w="100%">
+            <Button variant="primary" onClick={onClose}>
+              Close
+            </Button>
+            {(application?.status === 2 || application?.status === 0) && (
+              <Button variant="accept" onClick={accept}>
+                Accept
+              </Button>
+            )}
+            {(application?.status === 2 || application?.status === 1) && (
+              <Button variant="secondary" onClick={pending}>
+                Pending
+              </Button>
+            )}
+            {application?.status === 0 && (
+              <Button variant="decline" onClick={reject}>
+                Reject
+              </Button>
+            )}
+          </SimpleGrid>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
